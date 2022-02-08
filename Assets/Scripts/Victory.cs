87382982e4c8497ee6952d1830ca4428e164c650
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-02-08 17:33:18 +0100

    Highscores yey :trollface:

--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Victory : MonoBehaviour
 {
     public Button menuButton;
+    public TMP_InputField pseudoField;
     public void Setup(){
         gameObject.SetActive(true);
-        menuButton.onClick.AddListener(ReturnToMenu);
+        menuButton.onClick.AddListener(ReturnToMenuButton);
     }
-    public void ReturnToMenu()
+    public void ReturnToMenuButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    private void SaveScore()
+    {
+        
     }
 }
