1688f407cf51f9013ad3cfbf0af067450f5f8f67
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-02-12 15:57:19 +0100

    RestClient added to the project

--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using Proyecto26;
 
 public class Victory : MonoBehaviour
 {
@@ -12,13 +13,22 @@ public class Victory : MonoBehaviour
     public void Setup(){
         gameObject.SetActive(true);
         menuButton.onClick.AddListener(ReturnToMenuButton);
+        pseudoField.onSubmit.AddListener(ReturnToMenuField);
     }
     public void ReturnToMenuButton()
     {
+        if (pseudoField.text!="")
+        {
+            
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
-    private void SaveScore()
+    private void ReturnToMenuField(string oui)
     {
-        
+        ReturnToMenuButton();
+    }
+    private void SaveScoreToDatabase()
+    {
+        //RestClient.Post("https://demineur-3d-default-rtdb.europe-west1.firebasedatabase.app/.json");
     }
 }
