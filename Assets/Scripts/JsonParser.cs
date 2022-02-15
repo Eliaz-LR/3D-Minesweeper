6801121dcc535b665a818ce77ad3409bac23f001
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-02-15 16:38:48 +0100

    start of the parsing

---
+++ Assets/Scripts/JsonParser.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class JsonParser
+{
+    public static Highscore[] Parse(string toParse)
+
+    {
+        Highscore[] scoreArray= new Highscore[0];
+
+        return scoreArray;
+    }
+}
