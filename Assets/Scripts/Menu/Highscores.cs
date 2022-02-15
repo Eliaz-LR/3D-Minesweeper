bdd33e535f4ac1e0f66aef25075934b43db091b9
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-02-15 11:52:10 +0100

    IT WORKS NICELY YEY

--- Assets/Scripts/Menu/Highscores.cs
+++ Assets/Scripts/Menu/Highscores.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
+using Proyecto26;
 using TMPro;
 
 public class Highscores : MonoBehaviour
@@ -13,21 +13,13 @@ public class Highscores : MonoBehaviour
     private void Awake()
     {
         entryTemplate = transform.Find("HighscoreEntryTemplate");
-        Debug.Log(RetriveFromDatabase("easy"));
+        RetriveFromDatabase("easy");
     }
-    private IEnumerable RetriveFromDatabase(string difficulty)
+    private void RetriveFromDatabase(string difficulty)
     {
-        string URL="https://demineur-3d-default-rtdb.europe-west1.firebasedatabase.app/";
-        UnityWebRequest scoreInfoRequest = UnityWebRequest.Get(URL+difficulty+".json");
-
-        yield return scoreInfoRequest.SendWebRequest();
-
-        if (scoreInfoRequest.result == UnityWebRequest.Result.ConnectionError || scoreInfoRequest.result == UnityWebRequest.Result.ProtocolError)
+        RestClient.Get("https://demineur-3d-default-rtdb.europe-west1.firebasedatabase.app/"+difficulty+".json").Then(response =>
         {
-            Debug.LogError(scoreInfoRequest.error);
-            yield break;
-        }
-        json = scoreInfoRequest.downloadHandler.text;
-        Debug.Log(json);
+            Debug.Log(response.Text);
+        }).Catch(err=> Debug.LogError(err.Message));
     }
 }
