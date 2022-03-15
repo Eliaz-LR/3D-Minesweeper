c883f4f963ddb41630bc32f2c92239ae630c484a
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-03-15 16:19:16 +0100

    Memory of the settings using 2 different systems

--- Assets/Scripts/Menu/SettingMenu.cs
+++ Assets/Scripts/Menu/SettingMenu.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public GameObject volSlider;
+    public GameObject sensiSlider;
+
+
+    private void Awake()
+    {
+        volSlider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("volume");
+        sensiSlider.GetComponent<Slider>().value = StaticSettings.mouseSensitivity;
+    }
 
     public void SetVolume(float volume)
     {
+        PlayerPrefs.SetFloat("volume", volume);
         audioMixer.SetFloat("volume", volume);
     }
 
